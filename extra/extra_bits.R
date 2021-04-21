9741d818ba7af9a76179e6ced388795bf365ec39
jkubale <jkubale@umich.edu>
2021-04-21 09:13:16 -0400

    remove twitter feed from home pg

---
+++ extra/extra_bits.R
@@ -0,0 +1,3 @@
+# embed twitter feed
+<a class="twitter-timeline" data-width="300" data-height="300" href="https://twitter.com/JKubale?ref_src=twsrc%5Etfw">Tweets by JKubale</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  
